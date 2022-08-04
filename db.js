2a8e5b227724ce64fc73ba2fa5ac0c25cdaaf3ef
Raja Partogi <36397691+RajaPartogi@users.noreply.github.com>
2022-08-04 08:40:36 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,5 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { marshall } = require("@aws-sdk/util-dynamodb");
 
-const client = new DynamoDBClient(clientParams);
+const client = new DynamoDBClient({});
 
-const params = {
-    TableName: "Table",
-    Item: marshall({
-      HashKey: "hashKey",
-      NumAttribute: 1,
-      BoolAttribute: true,
-      ListAttribute: [1, "two", false],
-      MapAttribute: { foo: "bar" },
-      NullAttribute: null,
-    }),
-  };
-  
-  await client.putItem(params);
+module.exports = client;
